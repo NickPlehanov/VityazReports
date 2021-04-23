51a3be433381b013b6899dd056f5f12bb11f3594
pna <nick.plehanov@gmail.com7>
2021-04-23 17:03:01 +0500

    реализовано отображение техников на карте с расчетом расстояния между точками отметок

---
+++ Models/ServicemanOnMapViewModel/ServicemansPoints.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VityazReports.Models.ServicemanOnMapViewModel {
+    public class ServicemansPoints {
+        public ServicemansPoints(int objectNumber, DateTime income, DateTime outcome, int order, double distance) {
+            ObjectNumber = objectNumber;
+            Income = income;
+            Outcome = outcome;
+            Order = order;
+            Distance = distance;
+        }
+
+        public int ObjectNumber { get; set; }
+        public DateTime Income { get; set; }
+        public DateTime Outcome { get; set; }
+        public int Order { get; set; }
+        public double Distance { get; set; }
+    }
+}
--- ViewModel/ServicemanOnMapViewModel.cs
+++ ViewModel/ServicemanOnMapViewModel.cs
@@ -8,9 +8,13 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
 using VityazReports.Data;
 using VityazReports.Helpers;
 using VityazReports.Models;
+using VityazReports.Models.ServicemanOnMapViewModel;
+using GeoCoordinatePortable;
 
 namespace VityazReports.ViewModel {
     public class ServicemanOnMapViewModel : BaseViewModel {
@@ -85,6 +89,82 @@ namespace VityazReports.ViewModel {
             }
         }
         /// <summary>
+        /// Определяет видимость плавающей области с объектами техников
+        /// </summary>
+        private bool _PointsByServicemanFlyoutVisible;
+        public bool PointsByServicemanFlyoutVisible {
+            get => _PointsByServicemanFlyoutVisible;
+            set {
+                _PointsByServicemanFlyoutVisible = value;
+                OnPropertyChanged(nameof(PointsByServicemanFlyoutVisible));
+            }
+        }
+        private ObservableCollection<ServicemansPoints> _Points = new ObservableCollection<ServicemansPoints>();
+        public ObservableCollection<ServicemansPoints> Points {
+            get => _Points;
+            set {
+                _Points = value;
+                OnPropertyChanged(nameof(Points));
+            }
+        }
+
+        private ServicemansPoints _SelectedPoint;
+        public ServicemansPoints SelectedPoint {
+            get => _SelectedPoint;
+            set {
+                if (_SelectedPoint == null && value != null) {
+                    var _y = gmaps_contol.Markers.Where(x => x.Tag.ToString() == value.Order.ToString());
+                    if (_y == null)
+                        return;
+                    foreach (var item in _y) {
+                        GMapMarker _m = item as GMapMarker;
+                        if (_m == null)
+                            return;
+                        _m.ZIndex = 1000;
+                        Ellipse _ellipse = _m.Shape as Ellipse;
+                        _ellipse.StrokeThickness = 12;
+                        _ellipse.Stroke = Brushes.Green;
+                    }
+                }
+                if (_SelectedPoint != null) {
+                    if (_SelectedPoint != value) {
+                        if (value != null) {
+                            var y = gmaps_contol.Markers.Where(x => x.Tag.ToString() == _SelectedPoint.Order.ToString());
+                            if (y == null)
+                                return;
+                            foreach (var item in y) {
+                                GMapMarker m = item as GMapMarker;
+                                if (m == null)
+                                    return;
+                                m.ZIndex = 1;
+                                Ellipse ellipse = m.Shape as Ellipse;
+                                ellipse.StrokeThickness = 7.5;
+                                ellipse.Stroke = ellipse.AllowDrop ? Brushes.Red : Brushes.Blue;
+                            }
+                            var _y = gmaps_contol.Markers.Where(x => x.Tag.ToString() == value.Order.ToString());
+                            if (_y == null)
+                                return;
+                            foreach (var item in _y) {
+                                GMapMarker _m = item as GMapMarker;
+                                if (_m == null)
+                                    return;
+                                _m.ZIndex = 1000;
+                                Ellipse _ellipse = _m.Shape as Ellipse;
+                                _ellipse.StrokeThickness = 12;
+                                _ellipse.Stroke = Brushes.Green;
+                            }
+                        }
+                    }
+                }
+
+                _SelectedPoint = value;
+                //if (_SelectedPoint != null) {
+                //    var markers = gmaps_contol.Markers.Where(x => x.Tag.ToString() == _SelectedPoint.Order.ToString());
+                //}
+                OnPropertyChanged(nameof(SelectedPoint));
+            }
+        }
+        /// <summary>
         /// Инициализируем контрол для карты
         /// </summary>
         private RelayCommand _InitializeMapControl;
@@ -134,6 +214,7 @@ namespace VityazReports.ViewModel {
                               //&& !soc.SocOutcomeLongitude.Equals("0")
                               select new { soc, soeb }
                 ).AsNoTracking().ToList();
+                ListDates.Clear();
                 if (coords.Count() <= 0) {
                     notificationManager.Show(new NotificationContent() { Type = NotificationType.Error, Title = "Ошибка", Message = "Для выбранного техника не удалось найти координаты" });
                     return;
@@ -142,7 +223,7 @@ namespace VityazReports.ViewModel {
                 if (l_dates.Count() <= 0)
                     return;
                 foreach (var item in l_dates) {
-                    ListDates.Add(item.Value.Date.AddHours(5).Date);
+                    ListDates.Add(item.Value.AddHours(5).Date);
                 }
             }, obj => SelectedServiceman != null);
         }
@@ -155,19 +236,58 @@ namespace VityazReports.ViewModel {
                 var coords = (from soc in context.ServiceOrderCoordinates
                               join soeb in context.NewServiceorderExtensionBase on soc.SocServiceOrderId equals soeb.NewServiceorderId
                               where soeb.NewServicemanServiceorder == SelectedServiceman.NewServicemanId
-                              && soeb.NewDate.Value.Date == SelectedDate.Value.Date
+                              && soeb.NewDate.Value.Date == SelectedDate.Value.AddHours(-5).Date
                                 && !string.IsNullOrEmpty(soc.SocIncomeLatitude)
                                 && !string.IsNullOrEmpty(soc.SocIncomeLongitude)
                                 && !string.IsNullOrEmpty(soc.SocOutcomeLatitide)
                                 && !string.IsNullOrEmpty(soc.SocOutcomeLongitude)
-                                //&& !soc.SocIncomeLatitude.Equals("0")
-                                //&& !soc.SocIncomeLongitude.Equals("0")
-                                //&& !soc.SocOutcomeLatitide.Equals("0")
-                                //&& !soc.SocOutcomeLongitude.Equals("0")
                               select new { soc, soeb }
                               ).AsNoTracking().ToList();
                 if (coords.Count <= 0)
                     return;
+                Points.Clear();
+                gmaps_contol.Markers.Clear();
+                List<PointLatLng> points = new List<PointLatLng>();
+                int counter = 1;
+                foreach (var item in coords) {
+                    //пришёл
+                    GMapMarker marker = new GMapMarker(new PointLatLng(Convert.ToDouble(item.soc.SocIncomeLatitude), Convert.ToDouble(item.soc.SocIncomeLongitude))) {
+                        Shape = new Ellipse {
+                            Width = 20,
+                            Height = 20,
+                            Stroke = Brushes.Red,
+                            StrokeThickness = 4.5,
+                            ToolTip = string.Format("{0} - {1}", item.soeb.NewNumber, item.soeb.NewName),
+                            AllowDrop = true
+                        }
+                    };
+                    marker.Tag = counter;
+                    points.Add(new PointLatLng(Convert.ToDouble(item.soc.SocIncomeLatitude), Convert.ToDouble(item.soc.SocIncomeLongitude)));
+                    gmaps_contol.Markers.Add(marker);
+                    //ушёл
+                    marker = new GMapMarker(new PointLatLng(Convert.ToDouble(item.soc.SocOutcomeLatitide), Convert.ToDouble(item.soc.SocOutcomeLongitude))) {
+                        Shape = new Ellipse {
+                            Width = 20,
+                            Height = 20,
+                            Stroke = Brushes.Blue,
+                            StrokeThickness = 4.5,
+                            ToolTip = string.Format("{0} - {1}", item.soeb.NewNumber, item.soeb.NewName),
+                            AllowDrop = false
+                        }
+                    };
+                    marker.Tag = counter;
+                    points.Add(new PointLatLng(Convert.ToDouble(item.soc.SocOutcomeLatitide), Convert.ToDouble(item.soc.SocOutcomeLongitude)));
+                    gmaps_contol.Markers.Add(marker);
+                    GeoCoordinate c1 = new GeoCoordinate(Convert.ToDouble(item.soc.SocIncomeLatitude), Convert.ToDouble(item.soc.SocIncomeLongitude));
+                    GeoCoordinate c2 = new GeoCoordinate(Convert.ToDouble(item.soc.SocOutcomeLatitide), Convert.ToDouble(item.soc.SocOutcomeLongitude));
+                    Points.Add(new ServicemansPoints((int)item.soeb.NewNumber, (DateTime)item.soeb.NewIncome.Value.AddHours(5), (DateTime)item.soeb.NewOutgone.Value.AddHours(5), counter, c1.GetDistanceTo(c2)));
+                    counter++;
+                }
+                PointsByServicemanFlyoutVisible = Points.Count > 0;
+                //MapRoute route = new MapRoute(points, "1");
+                //route.Tag = "route";
+                //GMapRoute gmRoute = new GMapRoute(route.Points);
+                //gmaps_contol.Markers.Add(gmRoute);
             }, obj => SelectedDate != null);
         }
     }
